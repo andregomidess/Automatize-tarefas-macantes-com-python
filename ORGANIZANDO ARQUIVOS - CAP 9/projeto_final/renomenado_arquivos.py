b205077bbbce07aeabb3cb5f14f19d4c8e616d5b
andregomidess <andreluccag@gmail.com>
2022-06-10 17:34:28 -0300

    up

--- ORGANIZANDO ARQUIVOS - CAP 9/projeto_final/renomenado_arquivos.py
+++ ORGANIZANDO ARQUIVOS - CAP 9/projeto_final/renomenado_arquivos.py
@@ -1,14 +1,17 @@
 import os, re, shutil,send2trash, random
+##################################################################
 
-os.makedirs('/home/andre/aquivos_data_americana')   #pasta criada
-diretorio_de_trabalho = os.chdir('/home/andre/aquivos_data_americana')
-for i in range(10):
-    mes = str(random.randint(1, 12))
-    dia = str(random.randint(1, 30))
-    ano = str(random.randint(2010, 2021))
-    data = mes + '-' + dia + '-' + ano
-    arqs = open('galo %s.txt'%data, 'w')
-    
+#apenas para criar os arquivos
+
+#os.makedirs('/home/andre/aquivos_data_americana')   #pasta criada
+diretorio_de_trabalho = os.chdir('./home/andre/aquivos_data_americana')
+#for i in range(10):
+#    mes = str(random.randint(1, 12))
+#    dia = str(random.randint(1, 30))
+#    ano = str(random.randint(2010, 2021))
+#    data = mes + '-' + dia + '-' + ano
+#    arqs = open('galo%s.txt'%data, 'w')
+################################################################    
 data_regex = re.compile(r'''
                         ^(.*?)  # todo o texto antes
                         ((0|1)?\d)-  # meses 
@@ -17,4 +20,23 @@ data_regex = re.compile(r'''
                         (.*?)$  # todo o resto dps
             ''', re.VERBOSE)
 
+for nome_arq_americano in os.listdir(diretorio_de_trabalho):
+    
+    nomes = data_regex.search(nome_arq_americano)
+    if nomes == None:
+        continue
+    texto_antes = nomes.group(1)
+    meses_arq = nomes.group(2)
+    dia_arq = nomes.group(4)
+    ano_arq = nomes.group(6)
+    texto_depois = nomes.group(7)
+    
+    arq_data_europeia = texto_antes + dia_arq + '-' + meses_arq + '-' + ano_arq + texto_depois
+    
+    diretorio_de_trabalho_abs = os.path.abspath('.')
+    nome_arq_americano = os.path.join(diretorio_de_trabalho_abs, nome_arq_americano)
+    arq_data_europeia = os.path.join(diretorio_de_trabalho_abs, arq_data_europeia)
+    
+    print(f'Mudando "{nome_arq_americano}" para "{arq_data_europeia}"')
+    shutil.move(nome_arq_americano, arq_data_europeia)
     
