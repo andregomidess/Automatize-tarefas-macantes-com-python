1af61ce2a70f9d81fd7fdfd371549aaa93ad72de
andregomidess <andreluccag@gmail.com>
2022-06-13 23:50:27 -0300

    novo projeto

---
+++ ORGANIZANDO ARQUIVOS - CAP 9/projetos_finais/copia_seletiva.py
@@ -0,0 +1,13 @@
+import os, shutil
+
+
+def copia_seletiva(pasta_origem, pasta_destino):
+    pasta_origem = os.path.abspath(pasta_origem)
+    pasta_destino = os.path.abspath(pasta_destino)
+    for foldername, subfolders, filenames in os.walk(pasta_origem):
+        for filename in filenames:
+            if filename.endswith('.py'):
+                shutil.copy(os.path.join(foldername, filename), pasta_destino)
+
+if __name__ == '__main__':
+    copia_seletiva('/home/andre/teste', '/home/andre/aquii')                
